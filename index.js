e31ad8c284ae9329b424832bb99517bfb0c697c5
rotcivegaf <victorfage@gmail.com>
2019-07-12 23:17:11 -0300

    add pw

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ const PORT = process.env.PORT || 5000;
 abiProcessor();
 
 const redis = require('redis');
-const client = redis.createClient(18522, 'redis-18522.c16.us-east-1-2.ec2.cloud.redislabs.com');
+const client = redis.createClient(18522, 'redis-18522.c16.us-east-1-2.ec2.cloud.redislabs.com', {no_ready_check: true});
+
+client.auth('e4cUZUzXfoT3ePWprVLWZBydeeqvkH0u', function (err) {
+  if (err) throw err;
+});
 
 client.on('connect', function() {
   console.log('Connected to Redis');
