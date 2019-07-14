b3a6bb09be962ad5dec68b7063bac3b3e86be743
rotcivegaf <victorfage@gmail.com>
2019-07-14 03:25:38 -0300

    update created event

--- ETH/events/GamblingManager/Created(address,bytes32,address,bytes,uint256).js
+++ ETH/events/GamblingManager/Created(address,bytes32,address,bytes,uint256).js
@@ -1,17 +1,15 @@
+// event Created(address indexed _creator, bytes32 indexed _id, address _token, bytes _data, uint256 _nonce);
 const GamblingManager = require('../../build/contracts/GamblingManager.json');
 
 module.exports.signature = 'Created(address,bytes32,address,bytes,uint256)';
 module.exports.hexSignature = '0xc4317439075cf670016d9fff15c98babe21304c2c393e72a80d34d993779e70a';
 
 module.exports.process = async (log) => {
-  //const event = utils.getEvent(GamblingManager, 'Created(address,bytes32,address,bytes,uint256)', log);
-
-  return await commit(log);
-};
-
-async function commit(log) {
-  return {
-    key: log.id.slice(4).toString(),
-    data: log.toString()
+  const key = 'bet:' + log.topics[2];
+  const bet = {
+  	creator: log.topics[1]
+  	token: log.data.slice(0, 66)
   };
+
+  return { key: key, dataObject: bet };
 }
