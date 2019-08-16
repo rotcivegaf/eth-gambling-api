c46e5e20f491453a893f979bc46466cf5c56f67a
rotcivegaf <victorfage@gmail.com>
2019-08-16 02:23:58 -0300

    change model transfer signature

--- src/ETH/events/GamblingManager/ModelTransfer_35ab7603.js
+++ src/ETH/events/GamblingManager/ModelTransfer_35ab7603.js
@@ -1,16 +1,21 @@
 const GamblingManager = require('./GamblingManager.js');
 
-module.exports = class ModelTransfer_0beecb55 extends GamblingManager {
+module.exports = class ModelTransfer_35ab7603 extends GamblingManager {
   constructor(w3Utils, redisClient) {
     super(w3Utils, redisClient);
 
-    this.signature = 'ModelTransfer(bytes32,address,uint256)';
-    this.hexSignature = '0x0beecb5526acb0cbfa50f481ee6ddbcef5430db81beb1e4254a2113a25182e7f';
+    this.signature = 'ModelTransfer(bytes32,address,address,uint256)';
+    this.hexSignature = '0x35ab7603615e966c2fd7b33a8779b3227e56fc06c72e2f87fb419f9aac74caa8';
     this.inputs = [
       {
         'indexed':true,
         'name':'_id',
         'type':'bytes32'
+      },
+      {
+        'indexed':true,
+        'name':'_model',
+        'type':'address'
       },
       {
         'indexed':true,
