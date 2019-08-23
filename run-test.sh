6d5858d815aacdec5d2b09bbc88c96f2631e960a
rotcivegaf <victorfage@gmail.com>
2019-08-23 01:54:01 -0300

    config test

--- run-test.sh
+++ run-test.sh
@@ -2,4 +2,6 @@
 dockerc=docker-compose
 $dockerc -f docker-compose-test.yml down --remove-orphans &&
   $dockerc -f docker-compose-test.yml build &&
-  $dockerc -f docker-compose-test.yml up -d ganachecli redis processor
+  $dockerc -f docker-compose-test.yml up -d ganachecli redis &&
+  $dockerc -f docker-compose-test.yml up -d test-gambling-manager &&
+  $dockerc -f docker-compose-test.yml up -d processor
