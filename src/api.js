af62188f50e2a2df788aa11750fc1e8458b01035
rotcivegaf <victorfage@gmail.com>
2019-10-20 16:36:26 -0300

    add cors

--- src/api.js
+++ src/api.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 
 const PORT = process.env.PORT || 5000;
 
 module.exports = async (redis) => {
   const app = express();
+  app.use(cors());
   app.listen(PORT, () => console.info(`Listening on ${ PORT }`));
   // Last process block
   app.get('/lastProcessBlock', (req, res) => {
